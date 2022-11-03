b746a1ee81088c8db5cdb45ff52a0d131669e631
OPTRA <74641788+indiangamex@users.noreply.github.com>
2022-11-03 21:21:52 +0530

    Delete sciair directory

--- sciair/cityair/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class City(models.Model):
-    ip = models.SlugField(null=True)
-    city = models.CharField(max_length=50)
-    date = models.DateField()
-    time = models.TimeField(null=True)
-
-    def __str__(self):
-        return self.ip
